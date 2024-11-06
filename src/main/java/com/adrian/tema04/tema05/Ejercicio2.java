35d4996c3477ea30639788990f207ee944c50ccb
Adrian Hustea <adriannicolash3@gmail.com>
2024-11-06 15:21:43 +0100

    clase

--- src/main/java/com/adrian/tema04/tema05/Ejercicio2.java
+++ src/main/java/com/adrian/tema04/tema05/Ejercicio2.java
@@ -1,46 +1,50 @@
 package com.adrian.tema04.tema05;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String texto = scanner.nextLine();
+        System.out.println("En total hay: " + contadorVocales(texto)+ " vocales, y " +contadorConsonantes(texto) + " consonantes");
+    }
+
     /**
      * contara las vocales que hay en el texto
      * @param texto el texto
-     * @return devolvera la cantidad de vocales en el texto
+     * @return la cantidad de vocales en el texto
      */
     public static int contadorVocales(String texto){
         String bajado = texto.toLowerCase();
-        int cuentaVocales = 0;
+        int vocales = 0;
         char letra;
         char[] array = bajado.toCharArray();
         for (int i = 0; i <= array.length -1; i++){
             letra = array[i];
             switch (letra){
-                case 'a','e','i','o','u' -> cuentaVocales++;
+                case 'a','e','i','o','u' -> vocales++;
             }
         }
-        return cuentaVocales;
+        return vocales;
     }
 
     /**
      * contara las consonantes que hay en el texto
      * @param texto el texto
-     * @return devolvera la cantidad de consonantes en el texto
+     * @return la cantidad de consonantes en el texto
      */
     public static int contadorConsonantes(String texto){
         String bajado = texto.toLowerCase();
-        int cuentaConsonantes = 0;
+        int consonantes = 0;
         char letra;
         char[] array = bajado.toCharArray();
         for (int i = 0; i <= array.length -1; i++){
             letra = array[i];
             switch (letra){
-                case 'b','c','d','f','g','h','j','k','l','m','n','ñ','p','q','r','s','t','v','w','x','y','z' -> cuentaConsonantes++;
+                case 'b','c','d','f','g','h','j','k','l','m','n','ñ','p','q','r','s','t','v','w','x','y','z' -> consonantes++;
             }
         }
-        return cuentaConsonantes;
-    }
-
-    public static void main(String[] args) {
-        String texto = "hoLa Que Taleeñs";
-        System.out.println("En total hay: " + contadorVocales(texto)+ " vocales, y " +contadorConsonantes(texto) + " consonantes");
+        return consonantes;
     }
 }
