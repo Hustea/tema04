5d5a7c1ea2c04fdbc4511131301894e9953db025
Adrian Hustea <adriannicolash3@gmail.com>
2024-11-06 16:04:37 +0100

    clase

--- src/main/java/com/adrian/tema04/tema05/Ejercicio1.java
+++ src/main/java/com/adrian/tema04/tema05/Ejercicio1.java
@@ -1,6 +1,15 @@
 package com.adrian.tema04.tema05;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String texto = scanner.nextLine();
+        System.out.println(subirInicial(texto));
+    }
+
     public static String subirInicial(String texto){
         char letra = texto.charAt(0);
         String subir = String.valueOf(letra);
@@ -9,8 +18,4 @@ public class Ejercicio1 {
         return subido.concat(sobrante);
     }
 
-    public static void main(String[] args) {
-        String texto = "hoLa";
-        System.out.println(subirInicial(texto));
-    }
 }
